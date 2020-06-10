f58c822f17c0117ec1e1f25912229936f3363f76
wilsonsujames <43058541+wilsonsujames@users.noreply.github.com>
2020-06-10 13:46:33 +0800

    Add files via upload

---
+++ redwine/wine.py
@@ -0,0 +1,140 @@
+import pandas as pd
+import numpy as np
+import sklearn
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("./winequality-red.csv")
+
+# print(data.head())
+# print(data.columns)
+
+# sns.pairplot(data)
+from collections import Counter
+print(Counter(data['quality']))
+
+# sns.countplot(x='quality', data=data)
+
+
+
+reviews = []
+for i in data['quality']:
+    if i >= 1 and i <= 3:
+        reviews.append('1')
+    elif i >= 4 and i <= 7:
+        reviews.append('2')
+    elif i >= 8 and i <= 10:
+        reviews.append('3')
+data['Reviews'] = reviews
+
+x = data.iloc[:,:11]
+y = data['Reviews']
+
+# 看fixed acidity跟品質的相關性
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'fixed acidity', data = data)
+plt.show()
+
+# 標準化
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x = sc.fit_transform(x)
+
+# print(x)
+
+
+from sklearn.decomposition import PCA
+pca = PCA()
+x_pca = pca.fit_transform(x)
+
+print("X PCA")
+print(x_pca)
+
+# plot the graph to find the principal components
+plt.figure(figsize=(10,10))
+plt.plot(np.cumsum(pca.explained_variance_ratio_), 'ro-')
+plt.grid()
+plt.show()
+
+pca_new = PCA(n_components=8)
+x_new = pca_new.fit_transform(x)
+
+# print(x_new)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x_new, y, test_size = 0.25)
+
+print(x_train.shape)
+print(y_train.shape)
+print(x_test.shape)
+print(y_test.shape)
+
+
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import confusion_matrix, accuracy_score
+# lr = LogisticRegression(solver="liblinear",multi_class='auto')
+# lr.fit(x_train, y_train)
+# lr_predict = lr.predict(x_test)
+# #print confusion matrix and accuracy score
+# lr_conf_matrix = confusion_matrix(y_test, lr_predict)
+# lr_acc_score = accuracy_score(y_test, lr_predict)
+# print(lr_conf_matrix)
+# print(lr_acc_score*100)
+
+
+
+
+# from sklearn.tree import DecisionTreeClassifier
+# dt = DecisionTreeClassifier()
+# dt.fit(x_train,y_train)
+# dt_predict = dt.predict(x_test)
+# #print confusion matrix and accuracy score
+# dt_conf_matrix = confusion_matrix(y_test, dt_predict)
+# dt_acc_score = accuracy_score(y_test, dt_predict)
+# print(dt_conf_matrix)
+# print(dt_acc_score*100)
+
+
+# from sklearn.naive_bayes import GaussianNB
+# nb = GaussianNB()
+# nb.fit(x_train,y_train)
+# nb_predict=nb.predict(x_test)
+# #print confusion matrix and accuracy score
+# nb_conf_matrix = confusion_matrix(y_test, nb_predict)
+# nb_acc_score = accuracy_score(y_test, nb_predict)
+# print(nb_conf_matrix)
+# print(nb_acc_score*100)
+
+
+# from sklearn.ensemble import RandomForestClassifier
+# rf = RandomForestClassifier(n_estimators=100)
+# rf.fit(x_train, y_train)
+# rf_predict=rf.predict(x_test)
+#print confusion matrix and accuracy score
+# rf_conf_matrix = confusion_matrix(y_test, rf_predict)
+# rf_acc_score = accuracy_score(y_test, rf_predict)
+# print(rf_conf_matrix)
+# print(rf_acc_score*100)
+
+
+# SVC演算法
+# from sklearn.svm import SVC
+# lin_svc = SVC(gamma='auto')
+# lin_svc.fit(x_train, y_train)
+# lin_svc=rf.predict(x_test)
+# lin_svc_conf_matrix = confusion_matrix(y_test, rf_predict)
+# lin_svc_acc_score = accuracy_score(y_test, rf_predict)
+# print(lin_svc_conf_matrix)
+# print(lin_svc_acc_score*100)
+# rbf_svc = SVC(kernel='linear')
+# rbf_svc.fit(x_train, y_train)
+# rbf_svc=rf.predict(x_test)
+
+# rbf_svc_conf_matrix = confusion_matrix(y_test, rf_predict)
+# rbf_svc_acc_score = accuracy_score(y_test, rf_predict)
+# print(rbf_svc_conf_matrix)
+# print(rbf_svc_acc_score*100)
+
+
+
+# https://blog.csdn.net/m0_38061927/article/details/77198990
